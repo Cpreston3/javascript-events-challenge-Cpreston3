2ce89a6a3d3a4382ff0c934073ccd33a82fedc49
Casey Preston <caseypreston4@gmail.com>
2020-07-26 14:47:40 -0600

    added tier 2

--- public_html/scripts.js
+++ public_html/scripts.js
@@ -2,26 +2,36 @@ const firstPara = document.getElementById("p1");
 //console.log(firstParagraph);
 //tier 1.1 1.2
 firstPara.style.color = "green";
-firstPara.style.fontSize = "30px";
-//tier 1.3
-const button = document.getElementById("p2");
-button.addEventListener('click',() => {
+firstPara.style.fontSize = "20"
+    "px";
+//react to keyboard events
 
-})
 const secondPara = document.getElementById("p2");
-window.addEventListener("keydown", event => {
-    if (event.key == "m") {
+document.addEventListener("keydown", event => {
+    if (event.key === "m") {
         secondPara.style.background = "yellow";
     }
 });
-window.addEventListener("keyup", event => {
-    if (event.key == "m") {
+document.addEventListener("keyup", event => {
+    if (event.key === "m") {
         secondPara.style.background = "";
     }
 });
-//tier 2 find and replace
- const thirdPara =  document.getElementById("p3");
- thirdPara.innerText = "wizard for Life";
+//tier "find and replace" using button
+
+
+const button = document.querySelector("button");
+const thirdPara =  document.getElementById("p3");
+    button.addEventListener("click", () => {
+    thirdPara.innerText = "Wizard for Life";
+       });
+ //tier selecting one word in Dom
+
+ const ParaTwo = document.querySelector("p2");
+ document.documentElement("ParaTwo");
+
+
+
 
 
 
